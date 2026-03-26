d2baee01fe18d0f4c4ec1c1d35d6edfde2795347
unknown <adsbyalfred@protonmail.com>
2026-03-26 12:01:35 +0000

    Lower friction thank-you upsell CTA

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const OFFER_LINKS = {
   oto1: "https://buy.stripe.com/5kQ4gy86T64k0U8eKmbZe0x",
   downsell: "https://buy.stripe.com/00w14m86TdwMauIeKmbZe0yv",
   service:
-    "mailto:adsbyalfred@protonmail.com?subject=Fix%20More%20Leaks&body=Hi%2C%20I%27d%20like%20help%20fixing%20more%20leaks%20in%20my%20business.%0A%0ABusiness%20type%3A%0ALocation%3A%0ABiggest%20leaks%20I%20want%20fixed%3A%0AApprox.%20inbound%20enquiries%20per%20week%3A%0AWhat%20I%20need%20help%20with%3A%0A",
+    "mailto:adsbyalfred@protonmail.com?subject=LEAKS&body=LEAKS",
 };
 
 const DEFAULT_CURRENCY = "USD";
